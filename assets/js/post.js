b40cc01a39b1fec125ab6dc024120ebc99f2c094
woubuc <wouter@epicteddy.com>
2019-04-27 18:43:12 +0200

    Fixed & optimised JS

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,28 +1,41 @@
-(function readingProgress() {
+(function() {
 
 	// Get the title of the post to send to GA with our custom events
 	const postTitle = document.querySelector('.post-header__inner h1').textContent;
 
 	/**
-	 * Sends a download event to Google Analytics
-	 * @param file - Name of the file downloaded
+	 * Sends an event to Google Analytics
 	 */
-	function registerDownload(file) {
-		try {
+	function sendGaEvent(event, value) {
+		try { // Wrap in try/catch in case `gtag` isn't loaded yet or is blocked by an ad blocker
 			gtag('event', 'Post', {
-				event_category: 'download',
+				event_category: event,
 				event_label: postTitle,
-				value: pct,
+				value: value,
 			});
 		} catch (err) { }
 	}
 
-	document.querySelectorAll('.file__button > a').forEach(e => {
-		e.addEventListener('click',
-			() => registerDownload(e.getAttribute('download')),
-			{ capture: true, passive: true }
-			);
+
+
+	function onDownload(evt, el) {
+		evt.preventDefault();
+
+		const url = el.getAttribute('href');
+		sendGaEvent('download', url);
+
+		const frame = document.createElement('iframe');
+		frame.style.display = 'none';
+		frame.src = url;
+		document.body.appendChild(frame);
+
+		setTimeout(() => document.body.removeChild(frame), 10000);
+	}
+	document.querySelectorAll('.file__button > a').forEach(el => {
+		el.addEventListener('click', (evt) => onDownload(evt, el), true);
 	});
+
+
 
 	/**
 	 * Creates the reading progress element
@@ -34,22 +47,6 @@
 
 		document.body.appendChild(el);
 		return el;
-	}
-
-
-	/**
-	 * Sends a scroll event to Google Analytics
-	 * @param pct - Percentage scrolled as an integer from 0-100
-	 */
-	function registerScroll(pct) {
-		try { // Wrap GA call in try/catch so the rest of the script still works if GA is blocked
-			gtag('event', 'Post', {
-				event_category: 'scroll',
-				event_label: postTitle,
-				value: pct,
-			});
-		} catch (err) {
-		}
 	}
 
 	// Create the progress bar
@@ -68,18 +65,15 @@
 
 		if (pct > 0.5 && !sent50) {
 			sent50 = true;
-			registerScroll(50);
+			sendGaEvent('scroll', 50);
 		}
 
 		if (pct > 0.95 && !sent100) {
 			sent100 = true;
-			registerScroll(100);
+			sendGaEvent('scroll', 100);
 		}
 	}
 
 	// Attach event handler
-	window.addEventListener('scroll', onScroll, {
-		capture: true,
-		passive: true,
-	});
+	window.addEventListener('scroll', onScroll, { capture: true, passive: true });
 })();

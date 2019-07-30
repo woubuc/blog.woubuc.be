6a7556642f596e46deded700a2d51a4fbd4ddfd1
woubuc <wouter@epicteddy.com>
2019-07-30 08:51:57 +0200

    Added typescript syntax highlighting

--- src/highlight/index.js
+++ src/highlight/index.js
@@ -3,12 +3,14 @@ export default async function initHighlightJs(rootElement) {
 	const css = await import(/* webpackChunkName: 'hljs', webpackPreload: true */ 'highlight.js/lib/languages/css');
 	const xml = await import(/* webpackChunkName: 'hljs', webpackPreload: true */ 'highlight.js/lib/languages/xml');
 	const javascript = await import(/* webpackChunkName: 'hljs', webpackPreload: true */ 'highlight.js/lib/languages/javascript');
+	const typescript = await import(/* webpackChunkName: 'hljs', webpackPreload: true */ 'highlight.js/lib/languages/typescript');
 	const gdscript = await import(/* webpackChunkName: 'hljs', webpackPreload: true */ './gdscript');
 
 
 	hljs.registerLanguage('css', css.default);
 	hljs.registerLanguage('xml', xml.default);
 	hljs.registerLanguage('javascript', javascript.default);
+	hljs.registerLanguage('typescript', typescript.default);
 	hljs.registerLanguage('gdscript', gdscript.default);
 
 	rootElement.querySelectorAll('pre code').forEach(block => hljs.highlightBlock(block));

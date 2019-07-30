1724134f2befd268d84fb8ecabc5beb7c1edd3f5
woubuc <wouter@epicteddy.com>
2019-07-30 11:05:40 +0200

    Added basic 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,25 @@
+<template>
+	<layout narrow>
+		<g-image src="~/assets/images/404.jpg" width="680" height="320" />
+		<section>
+			<h1>404</h1>
+			<p>That page doesn't seem to exist. <g-link to="/">Back to home</g-link></p>
+		</section>
+	</layout>
+</template>
+
+<script>
+export default {
+	components: { },
+
+	metaInfo: {
+		title: 'Page not found',
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+img { border-radius: 6px }
+h1 { font-size: 1.8rem }
+section { padding: 10px 20px }
+</style>

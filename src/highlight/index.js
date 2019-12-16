063008fce957de0a6a7456849b1cae8259d971f0
woubuc <wouter@epicteddy.com>
2019-12-16 18:45:52 +0100

    💻 Add `bash` and `json` syntax highlighting

--- src/highlight/index.js
+++ src/highlight/index.js
@@ -2,6 +2,8 @@ export default async function initHighlightJs(rootElement) {
 	const hljs = await import(/* webpackChunkName: 'hljs', webpackPreload: true */ 'highlight.js/lib/highlight');
 	const css = await import(/* webpackChunkName: 'hljs', webpackPreload: true */ 'highlight.js/lib/languages/css');
 	const xml = await import(/* webpackChunkName: 'hljs', webpackPreload: true */ 'highlight.js/lib/languages/xml');
+	const bash = await import(/* webpackChunkName: 'hljs', webpackPreload: true */ 'highlight.js/lib/languages/bash');
+	const json = await import(/* webpackChunkName: 'hljs', webpackPreload: true */ 'highlight.js/lib/languages/json');
 	const javascript = await import(/* webpackChunkName: 'hljs', webpackPreload: true */ 'highlight.js/lib/languages/javascript');
 	const typescript = await import(/* webpackChunkName: 'hljs', webpackPreload: true */ 'highlight.js/lib/languages/typescript');
 	const gdscript = await import(/* webpackChunkName: 'hljs', webpackPreload: true */ './gdscript');
@@ -9,6 +11,8 @@ export default async function initHighlightJs(rootElement) {
 
 	hljs.registerLanguage('css', css.default);
 	hljs.registerLanguage('xml', xml.default);
+	hljs.registerLanguage('bash', bash.default);
+	hljs.registerLanguage('json', bash.default);
 	hljs.registerLanguage('javascript', javascript.default);
 	hljs.registerLanguage('typescript', typescript.default);
 	hljs.registerLanguage('gdscript', gdscript.default);
